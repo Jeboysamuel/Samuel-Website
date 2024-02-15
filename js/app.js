d366331ca4121b13f030924c0d93ef44d857b16e
jeboysamuto <spichon01@hotmail.com>
2024-02-15 17:00:38 +0100

    US2[] FIXES

--- js/app.js
+++ js/app.js
@@ -1,70 +1,83 @@
 const form = document.querySelector("form");
+const divLoading = document.getElementById("loading");
+const divCappie = document.getElementById("cappieDiv")
 const name = document.getElementById("name");
 const email = document.getElementById("email");
 const number = document.getElementById("number");
 const subject = document.getElementById("subject");
 const message = document.getElementById("message");
-const emailError = document.querySelector("#email + span.error");
 
-// email.addEventListener("input", (event) => {
-//     // Each time the user types something, we check if the
-//     // form fields are valid.
-//
-//     if (email.validity.valid) {
-//         // In case there is an error message visible, if the field
-//         // is valid, we remove the error message.
-//         emailError.textContent = ""; // Reset the content of the message
-//         emailError.className = "error"; // Reset the visual state of the message
-//     } else {
-//         // If there is still an error, show the correct error
-//         showError();
-//     }
-// });
-//
-// number.addEventListener("input", (event) => {
-//
-//     if (number.validity.patternMismatch) {
-//         number.setCustomValidity("Voer een geldig emailadres in!");
-//         number.reportValidity();
-//     } else {
-//         number.setCustomValidity("");
-//     }
-// });
+// Call the function to get a random number between 1 and 10
+let randomNumber1 = randomNummer(1, 9);
+let randomNumber2 = randomNummer(1,9);
+
+divCappie.innerHTML = '<label For="cappie">Controlevraag: Wat is ' + randomNumber1 +'+' + randomNumber2 + ' </label> <input class="info_input" type="text" name="cappie" id="cappie" required/>';
+const cappie = document.getElementById("cappie")
+
+
+email.addEventListener("input", (event) => {
+    if (email.validity.typeMismatch) {
+        email.setCustomValidity("Voer een geldig email in!");
+        email.reportValidity();
+    } else {
+        email.setCustomValidity("");
+    }
+});
+
+number.addEventListener("input", (event) => {
+    if (number.validity.patternMismatch) {
+        number.setCustomValidity("Voer een geldig nummer in!");
+        number.reportValidity();
+    } else {
+        number.setCustomValidity("");
+    }
+});
 
 form.addEventListener("submit", async (event) => {
     // Then we prevent the form from being sent by canceling the event
     event.preventDefault();
 
     // if the email field is valid, we let the form submit
-    // if (!email.validity.valid) {
-    //     // If it isn't, we display an appropriate error message
-    //     showError();
-    //     return;
-    // }
+    if (!email.validity.valid) {
+        // If it isn't, we display an appropriate error message
+        return;
+    }
+    if (!number.validity.valid) {
+        return;
+    }
+    let nummer = randomNumber1 + randomNumber2;
+    if (parseInt(cappie.value) !== nummer) {
+        return;
+    }
+    divLoading.innerHTML = '<i class="fas fa-spinner fa-spin contact-content__loading"></i>';
 
     let response = await fetch('https://localhost:7254/api/Mail', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({name: name.value, email: email.value, number: number.value, subject: subject.value, message: message.value})
-
-    });
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            name: name.value,
+            email: email.value,
+            number: number.value,
+            subject: subject.value,
+            message: message.value
+        })
+    }).then(response => {
+        alert("Gelukt");
+        divLoading.innerHTML = "";
+        form.reset()
+    }).catch(response => {
+        alert("Mislukt")
+        divLoading.innerHTML = "";
+        form.reset()
+    })
 });
 
-// function showError() {
-//     if (email.validity.valueMissing) {
-//         // If the field is empty,
-//         // display the following error message.
-//         emailError.textContent = "You need to enter an e-mail address.";
-//     } else if (email.validity.typeMismatch) {
-//         // If the field doesn't contain an email address,
-//         // display the following error message.
-//         emailError.textContent = "Entered value needs to be an e-mail address.";
-//     } else if (email.validity.tooShort) {
-//         // If the data is too short,
-//         // display the following error message.
-//         emailError.textContent = `E-mail should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
-//     }
-//
-//     // Set the styling appropriately
-//     emailError.className = "error active";
-// }
+function randomNummer(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+
+
+
