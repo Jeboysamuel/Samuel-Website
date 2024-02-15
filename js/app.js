90e86f9fa0d8c10765e6177751f2f1ce81061910
Jeboysamuto <spichon01@hotmail.com>
2024-02-15 20:50:09 +0100

    US2 laatste requirements

--- js/app.js
+++ js/app.js
@@ -7,9 +7,8 @@ const number = document.getElementById("number");
 const subject = document.getElementById("subject");
 const message = document.getElementById("message");
 
-// Call the function to get a random number between 1 and 10
-let randomNumber1 = randomNummer(1, 9);
-let randomNumber2 = randomNummer(1,9);
+let randomNumber1 = randomNummer(1, 4);
+let randomNumber2 = randomNummer(1,4);
 
 divCappie.innerHTML = '<label For="cappie">Controlevraag: Wat is ' + randomNumber1 +'+' + randomNumber2 + ' </label> <input class="info_input" type="text" name="cappie" id="cappie" required/>';
 const cappie = document.getElementById("cappie")
@@ -49,7 +48,7 @@ form.addEventListener("submit", async (event) => {
     if (parseInt(cappie.value) !== nummer) {
         return;
     }
-    divLoading.innerHTML = '<i class="fas fa-spinner fa-spin contact-content__loading"></i>';
+    divLoading.innerHTML = '<i class="fas fa-spinner fa-pulse contact-content__loading"></i>';
 
     let response = await fetch('https://localhost:7254/api/Mail', {
         method: 'POST',
@@ -62,18 +61,33 @@ form.addEventListener("submit", async (event) => {
             message: message.value
         })
     }).then(response => {
-        alert("Gelukt");
-        divLoading.innerHTML = "";
-        form.reset()
+        LoadingError("Succes");
+        form.reset();
     }).catch(response => {
-        alert("Mislukt")
-        divLoading.innerHTML = "";
-        form.reset()
+        LoadingError("Error");
+
     })
 });
 
 function randomNummer(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function LoadingError(status){
+    if(status === "Error"){
+        divLoading.innerHTML = '<label>' +
+            ' <input type="checkbox" class="alertCheckbox" autocomplete="off" /> ' +
+            '<div class="alert error"> <span class="alertClose">&nbsp;&nbsp;X</span> <span class="alertText">' +
+            'Het is niet gelukt om de email te sturen! Probeer het opniew <br class="clear"/></span> ' +
+            '</div> </label>';
+    }
+    if(status === "Succes"){
+        divLoading.innerHTML = '<label> ' +
+            '<input type="checkbox" class="alertCheckbox" autocomplete="off" /> ' +
+            '<div class="alert success"> <span class="alertClose">&nbsp;&nbsp;X</span> <span class="alertText">' +
+            'De email is succesvol verzonden!<br class="clear"/></span> ' +
+            '</div> </label>';
+    }
 }
 
 
